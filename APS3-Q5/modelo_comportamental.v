6ebd4d3f0868b289c916d3e29b0a9f93b0932ba2
Marcos Morais <5228.morais@gmail.com>
2025-12-10 15:18:18 -0300

    Update semaforo.v

--- APS3-Q5/modelo_comportamental.v
+++ APS3-Q5/modelo_comportamental.v
@@ -1,10 +1,9 @@
 `timescale 1ps/1ps
 
-module modelo_comportamental (
+module semaforo (
     input  wire clk,
     input  wire res,
     input  wire CAR,
-    input  wire TIMEOUT,
     output reg VERDE,
     output reg AMARELO,
     output reg VERMELHO
@@ -13,8 +12,13 @@ module modelo_comportamental (
     parameter S_VERDE    = 2'b00;
     parameter S_AMARELO  = 2'b01;
     parameter S_VERMELHO = 2'b10;
+	 parameter TAMARELO = 50000000; 
+	 parameter TVERMELHO = 750000000;
 
     reg [1:0] state, next_state;
+	 reg [32:0] counter_red;
+	 reg [32:0] counter_yellow;
+	 
 
     // Atualiza o estado na borda do clock
     always @(posedge clk or negedge res) begin
@@ -25,12 +29,27 @@ module modelo_comportamental (
     end
 
     // Define a lógica de transição de estados
-    always @(*) begin
+    always @(posedge clk) begin
         case(state)
-            S_VERDE:    next_state = (CAR) ? S_AMARELO : S_VERDE;
-            S_AMARELO:  next_state = S_VERMELHO;
-            S_VERMELHO: next_state = (TIMEOUT) ? S_VERDE : S_VERMELHO;
-            default:    next_state = S_VERDE;
+            S_VERDE: next_state = (CAR) ? S_AMARELO : S_VERDE;
+            
+				S_AMARELO:   
+				if (counter_yellow >= TAMARELO) begin
+					next_state = S_VERMELHO;
+					counter_yellow = 0;
+				end else begin
+					counter_yellow = counter_yellow + 1;
+				end
+				
+				S_VERMELHO:
+				if (counter_red >= TVERMELHO) begin
+					next_state = S_VERDE;
+					counter_red = 0;
+				end else begin	
+					counter_red = counter_red + 1;
+				end
+            
+				default: next_state = S_VERDE;
         endcase
     end
 
